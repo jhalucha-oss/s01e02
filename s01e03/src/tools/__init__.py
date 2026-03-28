5d9c4947fbd9d240518f506fda4b8f8a7d4ba981
jhalucha <j.halucha@sente.pl>
2026-03-28 17:22:25 +0100

    Czyszczenie repo

--- s01e03/src/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.tools.definitions import proxy_tools as tools
-from src.tools.handlers import handlers_proxy as handlers
-
-__all__ = ["tools", "handlers"]
--- src/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.tools.definitions import findhim_tools
-from src.tools.handlers import handlers_finding_suspect
-
-tools = findhim_tools
-handlers = handlers_finding_suspect
-
-__all__ = ["handlers", "tools", "findhim_tools", "handlers_finding_suspect"]
